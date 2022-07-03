fe271e338934a7698763aa6b3a278f9552632fda
calvingreen94 <calvingreen94@aol.com>
2022-07-03 11:20:45 -0500

    update

--- components/Header.js
+++ components/Header.js
@@ -3,17 +3,17 @@ import { Center, Flex, Text,Button } from "@chakra-ui/react"
 export default function Header({user, logout, isLoggingOut}){
     return(
     <header>
-        <Flex px="10" py="6" justifyContent="space-between" bg="purple.400" color="white">
+        <Flex px="10" py="6" justifyContent="space-between" bg="blackAlpha.800" color="white">
             <Center>
                 <Text fontSize="xl" fontWeight="bold">
-                    Tauros Dashboard: <br></br>
+                    Tauros Dashboard:
                 </Text>
             </Center>
             <Center>
                 <Text>
                     {user.getUsername()}
                 </Text>
-                    <Button ml="4" colorScheme="pink" onClick={logout} disable={isLoggingOut}>
+                    <Button ml="4" colorScheme="blackAlpha" onClick={logout} disable={isLoggingOut}>
                         Logout
                     </Button>
             </Center>
---
+++ components/Profile.js
@@ -0,0 +1,10 @@
+import CustomContainer from "./CustomContainer";
+
+export default function Profile(){
+    return(
+        <CustomContainer>
+            Now is okay
+        </CustomContainer>
+
+    )
+}
